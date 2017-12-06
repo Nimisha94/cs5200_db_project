0590dd6ae6a65dd5c1ae2541c0c5bace78ee794b
Nimisha <nimisha@Nimishas-MacBook-Pro.local>
2017-12-06 11:40:18 -0500

    adding package.json

---
+++ server.js
@@ -0,0 +1,25 @@
+var app = require('./express');
+var passport      = require('passport');
+var cookieParser  = require('cookie-parser');
+var session       = require('express-session');
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// configure a public directory to host static content
+app.use(app.express.static(__dirname + '/public'));
+
+app.use(session({
+    secret: 'secret123' //process.env.SESSION_SECRET
+}));
+app.use(cookieParser());
+app.use(passport.initialize());
+app.use(passport.session());
+
+// require ("./test/app.js")(app);
+
+var port = process.env.PORT || 4000;
+
+//require("./project/app");
+app.listen(port);
